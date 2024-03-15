481ddbd9e585ede519c85a9b4fa615b28946b499
suraj <kumarsurajverma6001@gmail.com>
2024-03-15 12:54:33 +0530

    final code

--- src/components/BlogComp.jsx
+++ src/components/BlogComp.jsx
@@ -5,34 +5,203 @@ import { IoMdTime } from "react-icons/io";
 const BlogComp = () => {
   return (
     <>
-
       <img
         src={icon}
         alt=""
         className="rotate-180 p-4 ml-8 h-[70px] cursor-pointer"
       />
 
-      <span className="flex flex-col w-[25%] bg-white p-6 rounded-2xl  shadow-2xl ">
-        <span className="flex-grow">
-          <img src={blog} alt="Blog" />
-          <p className="underline text-black text-2xl font-Rubik">Topic</p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
-            mollitia necessitatibus natus illo, cupiditate eum exercitationem
-            odio in fugit alias?
-          </p>
-          <span className="flex justify-between m-2 text-black font-Roboto">
-            <span className="flex items-center">
-              <img src={card_bg} alt="" className="mr-2" />
-              <p className="m-0">By TOI</p>
-            </span>
-            <span className="flex items-center text-black">
-              <IoMdTime className="mr-1 text-xl" />
-              <p className="m-0">07:00 PM</p>
+      <div className="flex justify-around  flex-wrap   gap-6 ">
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
             </span>
           </span>
         </span>
-      </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                <img src={card_bg} alt="" className="mr-2" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+        <span className="flex flex-col w-[28%] bg-white p-6 rounded-2xl  shadow-2xl ">
+          <span className="flex-grow">
+            <img src={blog} alt="Blog" className="mx-auto" />
+            <p className="underline text-black text-2xl font-Rubik">Topic</p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel
+              mollitia necessitatibus natus illo, cupiditate eum exercitationem
+              odio in fugit alias?
+            </p>
+            <span className="flex justify-between m-2 text-black font-Roboto">
+              <span className="flex items-center">
+                 <img src={blog} alt="Blog" className="mx-auto" />
+                <p className="m-0">By TOI</p>
+              </span>
+              <span className="flex items-center text-black">
+                <IoMdTime className="mr-1 text-xl" />
+                <p className="m-0">07:00 PM</p>
+              </span>
+            </span>
+          </span>
+        </span>
+      </div>
     </>
   );
 };
